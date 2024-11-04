8d2c51afdb3e55fae2805c4001cd624ade626302
aoife cassidy <aoife@enby.space>
2024-11-04 18:38:06 +0200

    feat: support multiple lock holders (#1)

--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,41 @@ export class Mutex {
     return willUnlock;
   }
 }
+
+export class MultiMutex {
+  private _queue: (() => void)[];
+  private _limit: number;
+  private _locks: number;
+
+  constructor(limit: number) {
+    this._queue = [];
+    this._limit = limit;
+    this._locks = 0;
+  }
+
+  isLocked() {
+    return this._locks >= this._limit;
+  }
+
+  async lock(): Promise<() => void> {
+    if (!this.isLocked()) {
+      this._locks++;
+      return this._unlock.bind(this);
+    }
+
+    return new Promise((resolve) => {
+      this._queue.push(() => {
+        this._locks++;
+        resolve(this._unlock.bind(this));
+      });
+    });
+  }
+
+  private _unlock() {
+    this._locks--;
+    if (this._queue.length && !this.isLocked()) {
+      const nextUnlock = this._queue.shift();
+      nextUnlock?.();
+    }
+  }
+}
